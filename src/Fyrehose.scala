d36a7fae5aa31087d1cec0149c4d65c85179a167
Paul Asmuth <paul@paulasmuth.com>
2012-05-15 01:33:46 +0200

    todos

--- src/Fyrehose.scala
+++ src/Fyrehose.scala
@@ -4,6 +4,11 @@ import java.util.Locale
 import java.util.Date
 import java.text.DateFormat
 
+// todo:
+//   > multiple streams kill each other since attachment will be overwritten. fix with sta
+//   > move endpoints hash to main object since multiplex will have it's own thread
+//   > kill endpoints on connection close!
+
 object Fyrehose{
   
   def main(args: Array[String]) : Unit = {
