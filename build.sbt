7ddbe5e15215539c278ca03157b1387cc89d9e1f
Paul Asmuth <paul@paulasmuth.com>
2012-05-14 23:24:35 +0200

    basic build setup

---
+++ build.sbt
@@ -0,0 +1,15 @@
+import AssemblyKeys._
+
+name := "Fyrehose"
+
+organization := "com.paulasmuth"
+
+version := "0.0.1"
+
+scalaSource in Compile <<= baseDirectory(_ / "src")
+
+mainClass in (Compile, run) := Some("com.paulasmuth.fyrehose.Fyrehose")
+
+assemblySettings
+
+scalaVersion := "2.9.1"
---
+++ src/Fyrehose.scala
@@ -0,0 +1,18 @@
+package com.paulasmuth.fyrehose
+
+import java.util.Locale
+import java.util.Date
+import java.text.DateFormat
+
+object Fyrehose{
+  
+  def main(args: Array[String]) : Unit = {
+    println("hello fyrehose")
+  }
+
+  def log(msg: String) = {
+    val now = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG, Locale.FRANCE)
+    println("[" + now.format(new Date()) + "] " + msg)
+  }
+
+}
