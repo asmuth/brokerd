5f88bdc6606e805c715025069fb84810e15f76bc
Paul Asmuth <paul@paulasmuth.com>
2012-06-11 16:38:12 +0200

    exceptions in the listener thread are fatal to the daemon

--- src/Listener.scala
+++ src/Listener.scala
@@ -6,27 +6,31 @@ import java.net._
 
 class Listener(port: Int){
 
-   val sock = new ServerSocket(port)
-   val clients = Executors.newFixedThreadPool(Fyrehose.NUM_THREADS_DISPATCH)
+  val sock = new ServerSocket(port)
+  val clients = Executors.newFixedThreadPool(Fyrehose.NUM_THREADS_DISPATCH)
 
-   Fyrehose.log("listening on port " + port.toString())
+  Fyrehose.log("listening on port " + port.toString())
 
-   var conn_opened = new java.util.concurrent.atomic.AtomicInteger;
-   var conn_closed = new java.util.concurrent.atomic.AtomicInteger;
+  var conn_opened = new java.util.concurrent.atomic.AtomicInteger;
+  var conn_closed = new java.util.concurrent.atomic.AtomicInteger;
 
-   def listen = {
-     clients.execute(new Runnable{ def run = {
-       while(true){
-         println(
-           "[STATUS] open: " + conn_opened.get.toString + 
-           ", closed: " + conn_closed.get.toString +
-           ", diff: " + (conn_opened.get - conn_closed.get).toString
-         )
+  def listen = {
+    clients.execute(new Runnable{ def run = {
+      while(true){
+        println(
+          "[STATUS] open: " + conn_opened.get.toString + 
+          ", closed: " + conn_closed.get.toString +
+          ", diff: " + (conn_opened.get - conn_closed.get).toString
+        )
 
-         val conn = new Endpoint(sock.accept())
-         clients.execute(conn)
-       }
-     }})
-   }
+        try{
+          val conn = new Endpoint(sock.accept())
+          clients.execute(conn)
+        } catch {
+          case e: Exception => Fyrehose.fatal(e.toString)
+        }
+      }
+    }})
+  }
 
 }
