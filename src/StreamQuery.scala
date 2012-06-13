12666159945bf53ea6765ed13b930dd282f9d632
Paul Asmuth <paul@paulasmuth.com>
2012-06-14 01:07:49 +0200

    refactored query parsing, implemented where_not

--- src/StreamQuery.scala
+++ src/StreamQuery.scala
@@ -2,19 +2,53 @@ package com.paulasmuth.fyrehose
 
 import scala.actors._
 
+// fixpaul: since(), until(), less/greater-than, regex, include exists
 class StreamQuery(raw: String) extends Query{
 
-  val X_VALIDATE = """^(([a-z]+\([^\)]*\)|and|or) *)+$"""
-  val X_EXTRACT  = """(([a-z]+)\(([^\)]*)\)|and|or)"""
+  val X_VALIDATE = """^(([a-z_]+\([^\)]*\)|and|or) *)+$"""
+  val X_EXTRACT  = """(([a-z_]+)\(([^\)]*)\)|and|or)"""
 
-  val x_stream = """^stream\(\)$""".r
-  val x_or     = """^or$""".r
-  val x_and    = """^and$""".r
-  val x_where  = """^where\(([^ ]+)"""
+  val X_STREAM = """^stream\(\)$""".r
+  val X_OR     = """^or$""".r
+  val X_AND    = """^and$""".r
 
-  val x_where_equals_str = (x_where + """ *= *'([^']*)'\)$""").r
-  val x_where_equals_int = (x_where + """ *= *([0-9]+)\)$""").r
-  val x_where_equals_dbl = (x_where + """ *= *([0-9]+\.[0-9]+)\)$""").r
+  trait X_WHERE {
+
+    def regex : String
+    def eval(v: String) : String => Boolean
+
+    def unapply(s: String) : Option[(String, (String => Boolean))] = {
+      val x_where_true = ("""^where\(([^ ]+)""" + regex).r
+      val x_where_false = ("""^where_not\(([^ ]+)""" + regex).r
+      s match {
+        case x_where_true(k: String, v: String) =>
+          return Some(((k, eval(v))))
+        case x_where_false(k: String, v: String) =>
+          return Some(((k, negated(eval(v)))))
+        case _ => 
+          return None
+      }
+    }
+
+    def negated(l: String => Boolean): String => Boolean =
+      (x: String) => l(x) unary_!
+
+  }
+
+  object X_EQUALS_STR extends X_WHERE {
+    def regex = """ *= *'([^']*)'\)$"""
+    def eval(v: String) = (x: String) => x == v
+  }
+
+  object X_EQUALS_INT extends X_WHERE {
+    def regex = """ *= *([0-9]+)\)$"""
+    def eval(v: String) = (x: String) => x.toInt == v.toInt
+  }
+
+  object X_EQUALS_DBL extends X_WHERE {
+    def regex = """ *= *([0-9]+\.[0-9]+)\)$"""
+    def eval(v: String) = (x: String) => x.toDouble == v.toDouble
+  }
 
 
   var recv : Actor = null
@@ -40,32 +74,32 @@ class StreamQuery(raw: String) extends Query{
     if (recv == null){
       this ! event
     } else if(fstack.eval(event)) {
+      // FIXPAUL: race condition ahead!
       recv ! new QueryResponseChunk(event.bytes)
       recv ! new QueryResponseChunk("\n".getBytes)
     }
 
 
-  // fixpaul: where_not, since(), until(), less/greater-than, regex, include exists
   private def parse(part: String) = {
     part match {
 
-      case x_stream() =>
+      case X_STREAM() =>
         ()
 
-      case x_or() =>
+      case X_OR() =>
         fstack = fstack.or()
 
-      case x_and() =>
+      case X_AND() =>
         fstack = fstack.and()
 
-      case x_where_equals_str(k: String, v: String) =>
-        fstack.push(k)((x: String) => x == v)
+      case X_EQUALS_STR(k: String, l: (String => Boolean)) =>
+        fstack.push(k)(l)
 
-      case x_where_equals_int(k: String, v: String) =>
-        fstack.push(k)((x: String) => x.toInt == v.toInt)
+      case X_EQUALS_INT(k: String, l: (String => Boolean)) =>
+        fstack.push(k)(l)
 
-      case x_where_equals_dbl(k: String, v: String) =>
-        fstack.push(k)((x: String) => x.toDouble == v.toDouble)
+      case X_EQUALS_DBL(k: String, l: (String => Boolean)) =>
+        fstack.push(k)(l)
 
       case _ =>
         throw new ParseException("invalid query part: " + part)
