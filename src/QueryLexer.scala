62d260ba761545806b89c286f16eba04546f3a97
Paul Asmuth <paul@paulasmuth.com>
2012-06-15 22:57:11 +0200

    moved query lexing logic to QueryLexer

--- src/QueryLexer.scala
+++ src/QueryLexer.scala
@@ -1,8 +1,34 @@
 package com.paulasmuth.fyrehose
 
-import scala.util.matching.Regex
+import scala.collection.mutable.ListBuffer
 
-class QueryLexer {
+class QueryLexer(recv: QueryParser) {
+
+  var buffer : String = ""
+  val stack  = ListBuffer[FQL_TOKEN]()
+
+  new FQL_ATOM +=: stack
+
+  def next(cursor: Char) : Unit = {
+
+    val next = stack.head.next(cursor, buffer)
+    println( stack.head.getClass.getName + " - " + buffer + " - " + cursor )
+
+    if (next != stack.head)
+      next +=: stack
+
+    if (stack.head.ready)
+      emit
+
+    if ((cursor != ' ') || (buffer.length > 0))
+      buffer  = stack.head.buffer(cursor, buffer)
+
+  }
+
+
+  def emit() : Unit = {
+    recv.emit(stack.remove(0)) // fixpaul unroll stack
+  }
 
 
 }
--- src/QueryParser.scala
+++ src/QueryParser.scala
@@ -1,37 +1,14 @@
 package com.paulasmuth.fyrehose
 
-import scala.actors.Actor
-import scala.actors.Actor._
-import scala.util.matching.Regex
-import scala.collection.mutable.ListBuffer
-
 class QueryParser {
 
-  var buffer : String = ""
-  var cursor : Char = 0
   var query  : Query = null
-  val stack  = ListBuffer[FQL_TOKEN]()
+  val lexer  = new QueryLexer(this)
 
   def parse(bdy: QueryBody) = {
 
-    new FQL_ATOM +=: stack
-
-    for (pos <- new Range(0, bdy.raw.size - 1, 1)) {
-      cursor  = bdy.raw(pos).toChar
-
-      val next = stack.head.next(cursor, buffer)
-      println( stack.head.getClass.getName + " - " + buffer + " - " + cursor )
-
-      if (next != stack.head)
-        stack.prepend(next)
-
-      if (stack.head.ready)
-        emit(stack.remove(0)) // fixpaul unroll stack
-
-      if ((cursor != ' ') || (buffer.length > 0))
-        buffer  = stack.head.buffer(cursor, buffer)
-
-    }
+    for (pos <- new Range(0, bdy.raw.size - 1, 1))
+      lexer.next(bdy.raw(pos).toChar)
 
     if (query == null)
       throw new ParseException("query must contain one of stream, info, etc.")
