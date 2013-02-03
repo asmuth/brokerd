712376fb5712e8ad54244809303d4f1ca1813f99
Paul Asmuth <paul@paulasmuth.com>
2013-02-03 21:52:06 +0100

    POC: naive channel implementation

--- fyrehose-poc/fyrehose_poc.rb
+++ fyrehose-poc/fyrehose_poc.rb
@@ -4,11 +4,23 @@ require "eventmachine"
 $: << ::File.expand_path("../../fyrehose-ruby/lib", __FILE__)
 require "fyrehose"
 
-class FyrehosePOC < EventMachine::Connection
+module FyrehosePOC
+
+  def self.channels
+    @@channels ||= Hash.new{ |h,k| h[k] = [] }
+  end
+
+end
+
+class FyrehosePOC::Connection < EventMachine::Connection
 
   def post_init
     @input_stream = Fyrehose::InputStream.new
     puts "connect"
+  end
+
+  def deliver(msg)
+    send_data "fnord"
   end
 
   def receive_data(chunk)
@@ -16,11 +28,33 @@ class FyrehosePOC < EventMachine::Connection
 
     @input_stream.each do |msg|
       puts msg.inspect
+
+      case msg[:type]
+
+        when :flags
+          if msg[:flags] & 1 == 1
+            FyrehosePOC.channels[msg[:channel]] << self
+          else
+            FyrehosePOC.channels[msg[:channel]].delete(self)
+          end
+
+        when :data
+          FyrehosePOC.channels[msg[:channel]].each do |recv|
+            recv.deliver(msg)
+          end
+
+      end
+    end
+  end
+
+  def unbind
+    FyrehosePOC.channels.each do |key, channel|
+      channel.delete(self)
     end
   end
 
 end
 
 EventMachine.run do
-  EventMachine.start_server("0.0.0.0", 2323, FyrehosePOC)
+  EventMachine.start_server("0.0.0.0", 2323, FyrehosePOC::Connection)
 end
